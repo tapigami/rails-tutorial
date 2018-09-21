d736d9661628965e8dc961483fa5c707519b4743
nishisuke <nishisuke13+git@gmail.com>
2018-09-21 21:45:13 +0900

    Add validation

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,3 +1,5 @@
 class Micropost < ApplicationRecord
   belongs_to :user
+
+  validates :content, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,9 @@ class User < ApplicationRecord
   has_many :microposts
 
   has_secure_password
+
+  validates :given_name, presence: true
+  validates :first_name, presence: true
+  validates :email_address, presence: true
+  # TODO: email format
 end
