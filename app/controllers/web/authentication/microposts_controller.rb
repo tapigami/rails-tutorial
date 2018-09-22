aaec90d078a3efe4dfcc408986aaf2cb0d18f656
nishisuke <nishisuke13+git@gmail.com>
2018-09-22 16:40:37 +0900

    rm unnecessary files

--- app/controllers/web/authentication/microposts_controller.rb
+++ app/controllers/web/authentication/microposts_controller.rb
@@ -1,24 +1,9 @@
 class Web::Authentication::MicropostsController < ::Web::AuthenticationController
-  before_action :set_micropost, only: [:show, :edit, :update, :destroy]
-
-  # GET /microposts
-  # GET /microposts.json
-  def index
-    @microposts = current_user.microposts
-  end
-
-  # GET /microposts/1
-  # GET /microposts/1.json
-  def show
-  end
+  before_action :set_micropost, only: %i[destroy]
 
   # GET /microposts/new
   def new
     @micropost = current_user.microposts.build
-  end
-
-  # GET /microposts/1/edit
-  def edit
   end
 
   # POST /microposts
@@ -32,20 +17,6 @@ class Web::Authentication::MicropostsController < ::Web::AuthenticationControlle
         format.json { render :show, status: :created, location: @micropost }
       else
         format.html { render :new }
-        format.json { render json: @micropost.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /microposts/1
-  # PATCH/PUT /microposts/1.json
-  def update
-    respond_to do |format|
-      if @micropost.update(micropost_params)
-        format.html { redirect_to @micropost, notice: 'Micropost was successfully updated.' }
-        format.json { render :show, status: :ok, location: @micropost }
-      else
-        format.html { render :edit }
         format.json { render json: @micropost.errors, status: :unprocessable_entity }
       end
     end
@@ -69,6 +40,6 @@ class Web::Authentication::MicropostsController < ::Web::AuthenticationControlle
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def micropost_params
-      params.require(:micropost).permit(:user_id, :content)
+      params.require(:micropost).permit(:content)
     end
 end
